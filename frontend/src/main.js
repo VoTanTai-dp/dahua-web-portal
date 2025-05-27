73fd05f73c256d8ff96d791ca4dc8dc575389862
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-27 08:53:50 +0700

    initial

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,14 @@
 import './assets/main.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
