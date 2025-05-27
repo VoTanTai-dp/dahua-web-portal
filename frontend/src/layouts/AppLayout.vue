81029f85401f28326a59768d70779d6c7e008fef
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-27 09:01:11 +0700

    Create AppLayout.vue

---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,21 @@
+Import
+<script setup>
+import NavigationBar from './NavigationBar.vue';
+
+</script>
+
+<template>
+    <div class="full-width-container">
+        <div class="row">
+            <!-- Sidebar -->
+            <navigation-bar></navigation-bar>
+
+            <!-- Main content -->
+            <div class="col-sm col-11 content">
+                <RouterView />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
