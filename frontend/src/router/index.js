ad5f283333bf96c10c96fc874497663567ed7db2
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-27 09:00:56 +0700

    Create index.js

---
+++ frontend/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import AppLayout from '@/layouts/AppLayout.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: AppLayout,
+    children: [
+      {
+        path: '/',
+        name: 'home',
+        component: () => import('@/views/HomePage.vue')
+      },
+      {
+        path: 'Database',
+        name: 'database',
+        component: () => import('@/views/DatabasePage.vue')
+      },
+    //   {
+    //     path: 'Devices',
+    //     name: 'devices',
+    //     component: () => import('@/views/DevicePage.vue')
+    //   },
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
