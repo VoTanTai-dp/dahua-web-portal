ef5ce109542becb5a15cc952c6f66d2c6ffb59c3
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-29 11:18:03 +0700

    Update AppLayout.vue

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -5,15 +5,15 @@ import NavigationBar from './NavigationBar.vue';
 </script>
 
 <template>
-    <div class="d-flex">
-            <!-- Sidebar -->
-             <div class="col-sm-2">
-                 <navigation-bar></navigation-bar>
-             </div>
-
-            <!-- Main content -->
-            <div class="col-sm-10">
-                <RouterView />
-            </div>
+    <div class="d-flex justify-content-center">
+        <!-- Sidebar -->
+        <div class="col-sm-2 justify-content-center align-items-center">
+            <navigation-bar></navigation-bar>
         </div>
+
+        <!-- Main content -->
+        <div class="col-sm-10">
+            <RouterView />
+        </div>
+    </div>
 </template>
