2f43dc65d65a1aef2f0187a5bcf3bfafe5afcf2c
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-05-30 10:41:52 +0700

    update bootstrap css

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -8,14 +8,24 @@ import NavigationBar from './NavigationBar.vue';
     <div class="d-flex justify-content-center">
         <div class="row col-12">
             <!-- Sidebar -->
-            <div class="col-sm-1 col-md-2 justify-content-center">
+            <div class="left-nav col-sm-1 col-md-2 justify-content-center">
                 <navigation-bar></navigation-bar>
             </div>
 
             <!-- Main content -->
-            <div class="col-12 col-sm-11 col-md-10 justify-content-center">
+            <div class="main col-12 col-sm-11 col-md-10 justify-content-center">
                 <RouterView />
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+.left-nav{
+    padding: 0;
+}
+
+.main{
+    padding: 0;
+}
+</style>
