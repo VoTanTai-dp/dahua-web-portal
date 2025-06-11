dbdef33a684f39a73e48455a94350d9c61baae81
VoTanTai-B2111948 <votantai27072003@gmail.com>
2025-06-11 08:22:20 +0700

    update homepage camera real-time stream video

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,51 +1,3 @@
-<!-- <script setup>
-</script>
-
-<template>
-    <div class="section camera mb-3">Camera View</div>
-
-    <div class="extension d-flex">
-        <div class="col-6 me-2">
-            <div class="section map">Map View</div>
-        </div>
-
-        <div class="col-6 row">
-            <div class="count d-flex mb-3">
-                <div class="col-6 section count__person">Person Count</div>
-                <div class="col-6 section count__vehicle mx-3">Vehicle Count</div>
-            </div>
-
-            <div class="sensor d-flex">
-                <div class="col-6 section sensor__temp">Temperature</div>
-                <div class="col-6 section sensor__humidity mx-3">Humidity</div>
-            </div>
-        </div>
-    </div>
-
-</template>
-
-<style scoped>
-.section {
-    background: #e9ecef;
-    /* margin-bottom: 20px; */
-    height: 200px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-weight: bold;
-    border: 1px solid #ccc;
-}
-
-.camera{
-    height: 700px;
-}
-
-.map{
-    height: 415px;
-}
-</style> -->
-
-
 <script setup>
 import { ref, onMounted } from 'vue'
 
@@ -57,8 +9,8 @@ onMounted(() => {
 
   ws.binaryType = 'arraybuffer'
 
-  ws.onopen = () => console.log('✅ WebSocket connected')
-  ws.onerror = (err) => console.error('❌ WebSocket error', err)
+  ws.onopen = () => console.log('WebSocket connected')
+  ws.onerror = (err) => console.error('WebSocket error', err)
 
   ws.onmessage = (event) => {
     const blob = new Blob([event.data], { type: 'image/jpeg' })
@@ -75,9 +27,8 @@ onMounted(() => {
 
 <template>
     <!-- Main -->
-    <div class="section camera mb-3 d-flex flex-column align-items-center justify-content-start p-2">
-        <h4 class="mb-2">📺 Dahua Camera Stream</h4>
-        <canvas ref="canvas" width="1280" height="720" style="border: 2px solid #444"></canvas>
+    <div class="section camera mb-3 d-flex flex-column align-items-center justify-content-start">
+        <canvas ref="canvas" width="1390" height="700" ></canvas>
     </div>
 
     <div class="extension d-flex">
@@ -107,7 +58,7 @@ onMounted(() => {
     align-items: center;
     justify-content: center;
     font-weight: bold;
-    border: 1px solid #ccc;
+    /* border: 1px solid #ccc; */
 }
 
 .camera {
